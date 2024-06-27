f9252140a98013d8f7f03e301e9dc744a5bf24c6
Matheus Santos <matheus.carneiro.ext@spms.min-saude.pt>
2024-06-27 16:46:06 +0100

    refactor(controller): changed to use @RequestParam

--- src/main/java/com/pt/weather/controller/CityController.java
+++ src/main/java/com/pt/weather/controller/CityController.java
@@ -17,8 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -43,7 +43,7 @@ public class CityController {
   private final CityService cityService;
 
   @PostMapping
-  public ResponseEntity<EntityModel<CityResponse>> createCity(@RequestBody String cityName) {
+  public ResponseEntity<EntityModel<CityResponse>> createCity(@RequestParam String cityName) {
     try {
       final var cityResponse = cityService.createCity(cityName);
       EntityModel<CityResponse> resource = EntityModel.of(cityResponse,
